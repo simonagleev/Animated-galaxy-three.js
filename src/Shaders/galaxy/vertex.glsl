dc3c7d9931d7a58496bce0fcbfe48d896a7af8c3
Simon <62551180+simonagleev@users.noreply.github.com>
2022-12-28 17:22:20 +0300

    Initial commit

---
+++ src/Shaders/galaxy/vertex.glsl
@@ -0,0 +1,44 @@
+
+uniform float uSize;
+uniform float uTime;
+
+attribute float aScale;
+attribute vec3 aRandomness;
+
+varying vec3 vColor;
+
+void main()
+{
+    /**
+     * Position
+     */
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    
+    // Spin      
+    float angle = atan(modelPosition.x, modelPosition.z);  // Galaxy shold be in the center of the scene;
+    float distanceToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceToCenter) * uTime;
+    angle += angleOffset;
+    modelPosition.x = cos(angle) * distanceToCenter;
+    modelPosition.z = sin(angle) * distanceToCenter;
+
+    // Randomness
+    modelPosition.x += aRandomness.x;
+    modelPosition.y += aRandomness.y;
+    modelPosition.z += aRandomness.z;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    /**
+     * Size
+     */
+    gl_PointSize = uSize * aScale;
+    gl_PointSize *= ( 1.0 / - viewPosition.z );
+
+     /**
+     * Color
+     */
+    vColor = color;
+}
